351e7e2d5198364b62b0038e48341624bc9025f4
Prokopenko S.A <serg-prokopenko@mail.ru>
2021-04-19 14:15:52 +0300

    Create project

---
+++ src/main/java/rusfedu/HospitalityNetwork/HospitalityNetworkApplication.java
@@ -0,0 +1,13 @@
+package rusfedu.HospitalityNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalityNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalityNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rusfedu/HospitalityNetwork/HospitalityNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package rusfedu.HospitalityNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
